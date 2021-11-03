bb8e01e7bbd975a9b5367a3e1ce7f57080ccb32c
jue131 <jue.13@yandex.ru>
2021-11-03 23:02:17 +0900

    фиксы

--- src/util/message.js
+++ src/util/message.js
@@ -4,11 +4,11 @@ async function repliedMessage(message) {
   if (!message.reference) return
   const msg = await message.channel.messages.fetch(message.reference.messageId).catch((e) => console.error(e))
   if (!msg || message.deleted) return
-  let content = msg.content.split(' ')
+  let content = msg.content.replace(/ +(?= )/g,'').split(' ')
 
   if (msg.embeds.length > 0) {
     if (msg.embeds[0].description) {
-      content = msg.embeds[0].description.split(' ')
+      content = msg.embeds[0].description.replace(/ +(?= )/g,'').split(' ')
     } else {
     //  errorParse('Found empty description in embed. Only embed descriptions are supported yet', message)
       return
